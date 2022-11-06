c5de49906479123be9169d6d6a877c768f7ea4fe
EasyZero <eminem54@naver.com>
2022-11-06 09:55:22 +0900

    feat: #1 깃 액션 배포 yml 추가
    test
    
    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello This is Ticketing Site!</h1>
+      <h1>Hello This is Ticketing Site!!</h1>
     </div>
   );
 }
