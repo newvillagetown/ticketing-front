54d1f04c93de5b05286ccac14b5023febcf1baef
EasyZero <eminem54@naver.com>
2022-11-13 11:58:02 +0900

    [#1] 배포 성공?

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1>즐거운 일이 끝나는 이유는 괴로운 일을 끝내기 위해서란다.</h1>
       <p>- 보노보노 -</p>
-      <span>12</span>
+      <h1>내일 월요일은 언제 끝나는거죠?</h1>
     </div>
   );
 }
