99589071395c09211674683ea231a9ef470cb2fe
EasyZero <eminem54@naver.com>
2022-11-14 20:45:37 +0900

    [#2] feat: 로그인 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
-
-export default function Home() {
+import React from "react";
+import HeaderOrganism from "src/components/organisms/header/Header.organism";
+const index = () => {
   return (
     <div>
-      <h1>즐거운 일이 끝나는 이유는 괴로운 일을 끝내기 위해서란다.</h1>
-      <p>- 보노보노 -</p>
-      <h1>내일 월요일은 언제 끝나는거죠?</h1>
-      <a href="https://dev-ticketing.breathings.net/v0.1/auth/google/signin">
-        로그인
-      </a>
+      <HeaderOrganism />
+      <a href="https://dev-ticketing.breathings.net/google/signin"></a>
     </div>
   );
-}
+};
+export default index;
