7d6254e0f05f859a4d4a1c0c1204edd4f49a91c0
EasyZero <eminem54@naver.com>
2022-11-14 20:52:53 +0900

    [#5] 로그인 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <h1>즐거운 일이 끝나는 이유는 괴로운 일을 끝내기 위해서란다.</h1>
@@ -11,4 +11,6 @@ export default function Home() {
       </a>
     </div>
   );
-}
+};
+
+export default Home;
