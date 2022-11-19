8708f0985aa3aaee219cdc3d4cf9cd6db5ba9869
EasyZero <eminem54@naver.com>
2022-11-19 14:24:06 +0900

    feat: #9 헤더 버튼 그라디언트로 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import { useEffect } from "react";
 import HomePage from "src/pages/home/Home.page";
+import MainLayout from "src/components/layouts/main/main.layout";
+import React from "react";
 
 const Home = () => {
   return <HomePage />;
+};
+
+Home.getLayout = function getLayout(page: React.ReactNode) {
+  return <MainLayout>{page}</MainLayout>;
 };
 
 export default Home;
