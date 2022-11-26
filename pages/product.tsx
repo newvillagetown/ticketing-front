658be68a98a5598f4dca89fa53a8b7c8f5e4ac6b
EasyZero <eminem54@naver.com>
2022-11-26 13:53:13 +0900

    feat: #15 상품 페이지   추가

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import LinkAtom from "src/components/atoms/link/Link.atom";
 import MainLayout from "src/components/layouts/main/main.layout";
+import ProductPage from "src/pages/product/Product.page";
 
 const Product = () => {
-  return (
-    <div>
-      아직 아무것도 없소요
-      <br />
-      <LinkAtom href="/">홈 화면으로</LinkAtom>
-    </div>
-  );
+  return <ProductPage />;
 };
 
 Product.getLayout = (page: React.ReactNode) => {
